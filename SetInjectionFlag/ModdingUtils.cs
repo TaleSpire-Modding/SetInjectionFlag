850c037a1bb57f67383d11a6e638552991c91590
brajm008 <ujkkju117@outlook.com>
2022-07-06 01:00:16 +0930

    Add project files.

---
+++ SetInjectionFlag/ModdingUtils.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using BepInEx;
+using UnityEngine.SceneManagement;
+using System;
+using BepInEx.Logging;
+using TMPro;
+
+namespace ModdingTales
+{
+    public static class ModdingUtils
+    {
+        private static BaseUnityPlugin parentPlugin;
+        private static ManualLogSource parentLogger;
+        
+        public static TextMeshProUGUI GetUITextByName(string name)
+        {
+            TextMeshProUGUI[] texts = UnityEngine.Object.FindObjectsOfType<TextMeshProUGUI>();
+            for (int i = 0; i < texts.Length; i++)
+            {
+                if (texts[i].name == name)
+                {
+                    return texts[i];
+                }
+            }
+            return null;
+        }
+
+        public static void Initialize(BaseUnityPlugin parentPlugin, ManualLogSource logger, bool startSocket=false)
+        {
+            AppStateManager.UsingCodeInjection = true;
+            ModdingUtils.parentPlugin = parentPlugin;
+            parentLogger = logger;
+            parentLogger.LogInfo("Inside initialize");
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        public static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            try
+            {
+                parentLogger.LogInfo("On Scene Loaded" + scene.name);
+                Debug.Log("Loading Scene: " + scene.name);
+                if (scene.name == "UI") {
+                    TextMeshProUGUI betaText = GetUITextByName("BETA");
+                    if (betaText)
+                    {
+                        betaText.text = "INJECTED BUILD - unstable mods";
+                    }
+                } else 
+                {
+                    TextMeshProUGUI modListText = GetUITextByName("TextMeshPro Text");
+                    if (modListText)
+                    {
+                        BepInPlugin bepInPlugin = (BepInPlugin)Attribute.GetCustomAttribute(ModdingUtils.parentPlugin.GetType(), typeof(BepInPlugin));
+                        if (modListText.text.EndsWith("</size>"))
+                        {
+                            modListText.text += "\n\nMods Currently Installed:\n";
+                        }
+                        modListText.text += "\n" + bepInPlugin.Name + " - " + bepInPlugin.Version;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                parentLogger.LogFatal(ex);
+            }
+        }
+    }
+}
---
+++ SetInjectionFlag/SetInjectionFlag.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using ModdingTales;
+
+namespace PluginUtilities
+{
+    [BepInPlugin(Guid, Name, Version)]
+    class SetInjectionFlag : BaseUnityPlugin
+    {
+        public const string Guid = "org.generic.plugins.setinjectionflag";
+        public const string Name = "Set Injection Flag Plugin";
+        public const string Version = "1.3.0.0";
+
+
+        void Awake()
+        {
+            Logger.LogInfo("In Awake for SetInjectionFlag Plug-in");
+
+            UnityEngine.Debug.Log("SetInjectionFlag Plug-in loaded");
+            ModdingUtils.Initialize(this, this.Logger);
+        }
+    }
+}
