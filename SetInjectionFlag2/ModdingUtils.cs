113103a44ccde826122b70cb57be96f7cf2c3984
brajm008 <ujkkju117@outlook.com>
2022-11-18 22:07:40 +1030

    2.4.0.0-Alpha

---
+++ SetInjectionFlag2/ModdingUtils.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using BepInEx;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections.Generic;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using JetBrains.Annotations;
+using TMPro;
+
+namespace ModdingTales
+{
+    public static class ModdingUtils
+    {
+        public enum LogLevel
+        {
+            [UsedImplicitly] Inherited,
+            [UsedImplicitly] None,
+            [UsedImplicitly] Low,
+            [UsedImplicitly] Medium,
+            [UsedImplicitly] High,
+            [UsedImplicitly] All,
+        }
+
+        public static ConfigEntry<LogLevel> LogLevelConfig { get; set; }
+
+        private static readonly Dictionary<(BaseUnityPlugin, string), ManualLogSource> ParentPlugins = new Dictionary<(BaseUnityPlugin, string), ManualLogSource>();
+        
+        public static TextMeshProUGUI GetUITextByName(string name)
+        {
+            TextMeshProUGUI[] texts = UnityEngine.Object.FindObjectsOfType<TextMeshProUGUI>();
+            for (int i = 0; i < texts.Length; i++)
+            {
+                if (texts[i].name == name)
+                {
+                    return texts[i];
+                }
+            }
+            return null;
+        }
+
+        public static void Initialize(BaseUnityPlugin parentPlugin, ManualLogSource logger, string author, bool startSocket = false)
+        {
+            AppStateManager.UsingCodeInjection = true;
+            ParentPlugins.Add((parentPlugin,author), logger);
+            logger.LogInfo("Inside initialize");
+        }
+
+        public static void Initialize(BaseUnityPlugin parentPlugin, ManualLogSource logger, bool startSocket=false)
+        {
+            AppStateManager.UsingCodeInjection = true;
+            ParentPlugins.Add((parentPlugin,""),logger);
+            logger.LogInfo("Inside initialize");
+        }
+
+        public static void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            foreach (var parentPlugin in ParentPlugins)
+            {
+                try
+                {
+                    parentPlugin.Value.LogInfo("On Scene Loaded " + scene.name);
+                    Debug.Log("Loading Scene: " + scene.name);
+                    if (scene.name == "UI")
+                    {
+                        var betaText = GetUITextByName("BETA");
+                        if (betaText)
+                        {
+                            betaText.text = "INJECTED BUILD - unstable mods";
+                        }
+                    }
+                    else
+                    {
+                        var modListText = GetUITextByName("TextMeshPro Text");
+                        if (!modListText) continue;
+                        var bepInPlugin =
+                            (BepInPlugin)Attribute.GetCustomAttribute(parentPlugin.Key.Item1.GetType(),
+                                typeof(BepInPlugin));
+                        if (modListText.text.EndsWith("</size>"))
+                        {
+                            modListText.text += "\n\nMods Currently Installed:\n";
+                        }
+
+                        modListText.text += string.IsNullOrWhiteSpace(parentPlugin.Key.Item2) ? 
+                            $"\n{bepInPlugin.Name} - {bepInPlugin.Version}" : 
+                            $"\n{parentPlugin.Key.Item2} {bepInPlugin.Name} - {bepInPlugin.Version}";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    parentPlugin.Value.LogFatal(ex);
+                }
+            }
+        }
+    }
+}
---
+++ SetInjectionFlag2/SetInjectionFlag.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using JetBrains.Annotations;
+using ModdingTales;
+using UnityEngine.SceneManagement;
+
+namespace PluginUtilities
+{
+    [BepInPlugin(Guid, Name, Version)]
+    public class SetInjectionFlag : BaseUnityPlugin
+    {
+        public const string Guid = "org.generic.plugins.setinjectionflag";
+        public const string Name = "Set Injection Flag Plugin";
+        public const string Version = "2.4.0.0";
+
+        
+        public static void DoConfig(ConfigFile config)
+        {
+            ModdingUtils.LogLevelConfig = config.Bind("Logging", "Level", ModdingUtils.LogLevel.Low);
+            if (ModdingUtils.LogLevelConfig.Value != ModdingUtils.LogLevel.Inherited) return;
+            ModdingUtils.LogLevelConfig.Value = ModdingUtils.LogLevel.None;
+            UnityEngine.Debug.Log("Logging level set to None, Inherited is for child plugins");
+        }
+
+        [UsedImplicitly]
+        private void Awake()
+        {
+            Logger.LogInfo("In Awake for SetInjectionFlag Plug-in");
+            UnityEngine.Debug.Log("SetInjectionFlag Plug-in loaded");
+            ModdingUtils.Initialize(this, Logger);
+            SceneManager.sceneLoaded += ModdingUtils.OnSceneLoaded;
+            DoConfig(Config);
+        }
+    }
+}
